6a94e1bddda06a2b96ba108082d63063fadbfd2b
Alexey Demchenko <alexeydemchenko@protonmail.com>
2018-02-26 16:33:33 +0200

    Adding main methods to CustomLinkedList interface

--- src/main/java/CustomLinkedList.java
+++ src/main/java/CustomLinkedList.java
@@ -1,2 +1,18 @@
+import java.util.Collection;
+
 public interface CustomLinkedList <T> extends Iterable<T> {
+
+    public boolean add(T t);
+
+    public boolean addAll(Collection<? extends T> c);
+
+    public void clear();
+
+    public T get(int index);
+
+    public T set(int index, T element);
+
+    public T remove(int index);
+
+    public int indexOf(Object obj);
 }
