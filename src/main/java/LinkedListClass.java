e56b1206eda752e0283a3e0aea61f78a2014c0eb
Alexey Demchenko <alexeydemchenko@protonmail.com>
2018-03-05 12:49:41 +0200

    Adding empty methods to the LinkedListClass structure

---
+++ src/main/java/LinkedListClass.java
@@ -0,0 +1,111 @@
+import java.util.*;
+
+public class LinkedListClass<T> implements CustomLinkedList<T> {
+
+    private int size = 0;
+    private Node headElement;
+    private Node tailElement;
+
+    public LinkedListClass(){
+
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    public <T1> T1[] toArray(T1[] a) {
+        return null;
+    }
+
+    public boolean add(T t) {
+
+        if(headElement == null){
+            headElement = new Node(t);
+        }
+
+
+
+        return false;
+    }
+
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    public boolean addAll(Collection<? extends T> c) {
+        return false;
+    }
+
+    public boolean addAll(int index, Collection<? extends T> c) {
+        return false;
+    }
+
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    public void clear() {
+
+    }
+
+    public T get(int index) {
+        return null;
+    }
+
+    public T set(int index, T element) {
+        return null;
+    }
+
+    public void add(int index, T element) {
+
+    }
+
+    public T remove(int index) {
+        return null;
+    }
+
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    public ListIterator<T> listIterator() {
+        return null;
+    }
+
+    public ListIterator<T> listIterator(int index) {
+        return null;
+    }
+
+    public List<T> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+}
